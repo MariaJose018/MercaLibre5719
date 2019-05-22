efcfcd687346f0aedf2d66ea89717ac5235c9232
bruno1304 <bruno130402@gmail.com>
2019-05-22 16:36:03 -0300

    Funcion de recaudación
    Hice una función para sacar la recaudación re atr

--- scripts/bd/MySQL/02 sf.sql
+++ scripts/bd/MySQL/02 sf.sql
@@ -1,3 +1,15 @@
+CREATE FUNCTION recaudacionPara (unidproducto INT, fechayhora1 DATETIME, fechayhora2 DATETIME)
+                                 RETURNS FLOAT
+BEGIN
+     DECLARE suma FLOAT;
+     SELECT SUM(precio)  INTO suma
+     FROM CompraVenta
+     WHERE idproducto = unidproducto
+     AND fechayhora BETWEEN fechayhora1 AND fechayhora2
+     RETURN suma;
+END
+
+
 CREATE FUNCTION recaudacionTotal(unidCliente INT, unafechayhora1 DATETIME, unafechayhora2 DATETIME)
                                  RETURNS FLOAT
 BEGIN 
