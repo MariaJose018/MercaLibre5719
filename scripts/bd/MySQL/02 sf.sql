fbf36b3311bde83caee61046937329863f75bd8b
Majo <49031852+MariaJose018@users.noreply.github.com>
2019-06-06 13:58:33 -0300

    Update 02 sf.sql
    1)Agregar delimiter.(Listo)

--- scripts/bd/MySQL/02 sf.sql
+++ scripts/bd/MySQL/02 sf.sql
@@ -1,22 +1,27 @@
+DELIMITER//
 CREATE FUNCTION recaudacionPara (unidproducto INT, fechayhora1 DATETIME, fechayhora2 DATETIME)
                                  RETURNS FLOAT
 BEGIN
      DECLARE suma FLOAT;
+     
      SELECT SUM(precio)  INTO suma
      FROM CompraVenta
      WHERE idproducto = unidproducto
-     AND fechayhora BETWEEN fechayhora1 AND fechayhora2
+     AND fechayhora BETWEEN fechayhora1 AND fechayhora2;
+     
      RETURN suma;
-END
+END//
 
-
+DELIMITER//
 CREATE FUNCTION recaudacionTotal(unidCliente INT, unafechayhora1 DATETIME, unafechayhora2 DATETIME)
                                  RETURNS FLOAT
 BEGIN 
      DECLARE suma FLOAT;
+     
      SELECT SUM(precio) INTO suma
      FROM CompraVenta
      WHERE idUsuario = unidUsuario
-     AND fechayhora1 BETWEEN fechayhora1 AND fechayhora2
+     AND fechayhora1 BETWEEN fechayhora1 AND fechayhora2;
+     
      RETURN suma;
-END
+END//
