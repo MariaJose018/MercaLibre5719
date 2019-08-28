f1e2c02cb7ed4161bcbd3ab7db7b86e045d2d5d7
bruno1304 <48721991+bruno1304@users.noreply.github.com>
2019-08-28 17:46:11 -0300

    TestProducto

--- Scr/CSharp/Test/TestProducto.cs
+++ Scr/CSharp/Test/TestProducto.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using MercaLibre5to7ma;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+
+
 namespace Test
 {
-    class TestProducto
+    [TestClass]
+    public class TestProducto
     {
+        Producto Jordan3 { get; set; }
+
+        [TestInitialize]
+
+        public void creacionProducto()
+        {
+            Jordan3 = new Producto() { Nombre = "Jordan 3" };
+            Jordan3.Nombre = "Air Jordan 3 OG";
+            Jordan3.Precio = 12000;
+            Jordan3.cantidad = 1;
+        }
+        [TestMethod]
+        public void cambiarPrecio()
+        {
+
+        }
+        
+
+
     }
 }
