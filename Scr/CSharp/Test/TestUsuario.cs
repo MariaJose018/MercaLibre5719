5c73db0a9492ab174dcf609103fc3d81d611b850
angelcr25 <49032121+angelcr25@users.noreply.github.com>
2019-08-29 14:54:08 -0300

    Update TestUsuario.cs
    Test en proceso

--- Scr/CSharp/Test/TestUsuario.cs
+++ Scr/CSharp/Test/TestUsuario.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Mercalibre15;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Test
 {
-    class TestUsuario
+    [TestClass]
+    public class TestUsuario
     {
+        Usuario usuario { get; set; }
+        [TestInitialize]
+        public void CrearUsuario()
+        {
+            usuario.Nombre = "Alberto";
+            usuario.Apellido = "Fernandez";
+            usuario.Telefono = 1134552177;
+            usuario.Nombredeusuario = "AlbertocuAbierto";
+            usuario.Email = "Alberto@gmail.com";
+            usuario.Contrasenia = "AlbertoyCristina";
+
+        }
+        [TestMethod]
+        public void AgregarProductosEnVenta()
+        {
+            usuario.AgeregarProductos();
+        }
     }
 }
