4869f6fc74038c58786e394dac8d70b58aecc58f
bruno1304 <bruno130402@gmail.com>
2019-11-14 14:32:27 -0300

    retoques en los Usuarios

--- scripts/bd/MySQL/03 Users y grants.sql
+++ scripts/bd/MySQL/03 Users y grants.sql
@@ -1,14 +1,13 @@
-"Usuario"
+# "Usuario"
 CREATE USER 'userLibre'@'%' identified by '1234';
 
 GRANT select, update(Nombre,Apellido,Telefono,Contrasenia,Email)on usuario to 'userLibre'@'%';
 GRANT select, update(Precio, Cantidad) on producto to 'userLibre'@'%';
 GRANT select on compraventa to 'userLibre'@'%';
 
-"Administrador"
+# "Administrador"
 CREATE USER 'Administrador'@'10.3.45.%' IDENTIFIED BY 'passadmin';
 
 GRANT SELECT,DELETE,UPDATE(Cantidad) ON producto TO 'Administrador'@'10.3.45.%';
 GRANT SELECT ON compraventa TO 'Administrador'@'10.3.45.%';
 GRANT SELECT,UPDATE(Nombre,Apellido,Telefono,Contrasenia,Email)ON usuario TO 'Administrador'@'10.3.45.%';
-
