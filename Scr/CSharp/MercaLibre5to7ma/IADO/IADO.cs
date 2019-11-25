2c76852aaa3066c5ef709c94a637fec313eae36c
angelcr25 <49032121+angelcr25@users.noreply.github.com>
2019-11-25 11:15:57 -0300

    Update IADO.cs

--- Scr/CSharp/MercaLibre5to7ma/IADO/IADO.cs
+++ Scr/CSharp/MercaLibre5to7ma/IADO/IADO.cs
@@ -11,7 +11,7 @@ namespace Mercalibre15.ADO
 
         void Aproductoventa(Compraventa compraVenta);
         List<Compraventa> obtenerventas();
-
+        void mProducto(Producto producto);
         void aproducto(Producto producto);
         List<Producto> obtenerproductos();
 
