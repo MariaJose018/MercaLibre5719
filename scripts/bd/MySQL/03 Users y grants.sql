9546b3076f638ca6e139b7bac368968d9f60cc51
gustavo1020 <gustavolores15@gmail.com>
2019-11-25 15:50:35 -0300

    Menues mejor organizados

--- scripts/bd/MySQL/03 Users y grants.sql
+++ scripts/bd/MySQL/03 Users y grants.sql
@@ -6,8 +6,15 @@ GRANT select, update(Precio, Cantidad) on producto to 'userLibre'@'%';
 GRANT select on compraventa to 'userLibre'@'%';
 
 # "Administrador"
-CREATE USER 'Administrador'@'10.3.45.%' IDENTIFIED BY 'passadmin';
+CREATE USER 'Administrador'@'10.3.120.%' IDENTIFIED BY 'passadmin';
 
-GRANT SELECT,DELETE,UPDATE(Cantidad) ON producto TO 'Administrador'@'10.3.45.%';
-GRANT SELECT ON compraventa TO 'Administrador'@'10.3.45.%';
-GRANT SELECT,UPDATE(Nombre,Apellido,Telefono,Contrasenia,Email)ON usuario TO 'Administrador'@'10.3.45.%';
+GRANT SELECT,DELETE,UPDATE(Cantidad) ON producto TO 'Administrador'@'10.3.120.%';
+GRANT SELECT ON compraventa TO 'Administrador'@'10.3.120.%';
+GRANT SELECT,UPDATE(Nombre,Apellido,Telefono,Contrasenia,Email)ON usuario TO 'Administrador'@'10.3.120.%';
+
+# "Usuario en LAN"
+CREATE USER 'userLibre'@'10.3.120.%' identified by '1234';
+
+GRANT select, update(Nombre,Apellido,Telefono,Contrasenia,Email)on usuario to 'userLibre'@'10.3.120.%';
+GRANT select, update(Precio, Cantidad) on producto to 'userLibre'@'10.3.120.%';
+GRANT select on compraventa to 'userLibre'@'10.3.120.%';
