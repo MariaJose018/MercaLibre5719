af83dfacef040b0088d6db5dbcc294103e272207
gustavo1020 <gustavolores15@gmail.com>
2019-12-04 17:36:24 -0300

    fhgfh
    fhfghf

---
+++ Scr/CSharp/MercaLibre5to7ma/Tipodepago.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+
+namespace Mercalibre15
+{
+    [Table("Producto")]
+    public class Tipodepago
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column("idtipodepago")]
+        public short Id { get; set; }
+
+        [ForeignKey("idvendedor"), Required]
+        public Usuario usuario { get; set; }
+
+        [ForeignKey("idcomprador"), Required]
+        public Compraventa Compraventa { get; set; }
+
+        [Column("Tipo de Pago"), StringLength(45), Required]
+        public string Nombre { get; set; }
+
+        [Column("fechadepago"), Required]
+        public DateTime fechadepago { get; set; }
+
+        
+
+
+    }
+}
